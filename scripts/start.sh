659dd49c21b456ffb0b77872d8e09fe718aaf5fb
严恺珺 <kaijun@fit2cloud.com>
2018-05-30 10:59:20 +0800

    修改部署文件

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 cd /opt/fit2cloud-demo
 echo going to start app
-nohup java -Djava.io.tmpdir=/opt/fit2cloud/tmp -jar .springcloud-eureka-server-ha-2.0.0-SNAPSHOT.jar  >> /var/log/springcloud-eureka.log 2>&1 &
+nohup java -Djava.io.tmpdir=/opt/fit2cloud/tmp -jar springcloud-eureka-server-ha-2.0.0-SNAPSHOT.jar  >> /var/log/springcloud-eureka.log 2>&1 &
 echo app started!
-ps aux | grep .springcloud-eureka-server-ha-2.0.0-SNAPSHOT.jar | grep -v grep
+ps aux | grep springcloud-eureka-server-ha-2.0.0-SNAPSHOT.jar | grep -v grep
--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-processesNum=`ps aux | grep .springcloud-eureka-server-ha-2.0.0-SNAPSHOT.jar  | grep -v grep | wc -l | sed 's/ //g'`
-ps aux | grep .springcloud-eureka-server-ha-2.0.0-SNAPSHOT.jar  | grep -v grep
+processesNum=`ps aux | grep springcloud-eureka-server-ha-2.0.0-SNAPSHOT.jar  | grep -v grep | wc -l | sed 's/ //g'`
+ps aux | grep springcloud-eureka-server-ha-2.0.0-SNAPSHOT.jar  | grep -v grep
 echo process number is $processesNum
 if [ "$processesNum" == "1" ];then
     echo app deployed successfully!
