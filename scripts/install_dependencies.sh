2fca7a7dfbba8887306a2cc269590b3db1cd03a2
严恺珺 <kaijun@fit2cloud.com>
2018-05-30 10:52:44 +0800

    修改部署文件

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,6 +3,8 @@
 echo install dependencies
 if [ -d /opt/fit2cloud-demo ];then
     echo /opt/fit2cloud-demo exists
+    rm -rf /opt/fit2cloud-demo/*
+    echo clean /opt/fit2cloud-demo
 else
     mkdir -p /opt/fit2cloud-demo
 fi
